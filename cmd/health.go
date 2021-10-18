e96a28f1f015fd32ca955956b2cdac21f0ad907e
Pieter De Clercq <pieterdeclercq@outlook.com>
2021-10-18 20:48:37 +0200

    Expose version via /health endpoint

--- cmd/health.go
+++ cmd/health.go
@@ -3,13 +3,15 @@ package cmd
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/thepieterdc/gopos/pkg/health"
+	"github.com/thepieterdc/gopos/version"
 	"net/http"
 )
 
 // HealthHandler handles the /health route.
 func HealthHandler(ctx echo.Context) error {
 	response := &health.Response{
-		Status: true,
+		Status:  true,
+		Version: version.VERSION,
 	}
 
 	// Send the response.
